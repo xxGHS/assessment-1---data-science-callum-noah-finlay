2e8f005d2f5fac0b8ce5e628c9a54ceaf0692a44
Gronkaa <callum.lane3@education.nsw.gov.au>
2023-03-31 03:22:16 +1100

    Removed unnecessary data, improved data cleaning, deleted unnecessary code

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ dfmodels = pd.read_csv('cars (Models).csv')
 df = df.drop_duplicates(subset="Model", keep="first")
 dfmodels = dfmodels.drop_duplicates(subset="Model", keep="first")
 
+# Remove rows with missing values
+df.dropna()
+
 brand = "Toyota"
 models = df.loc[df["Brand"] == brand]["Model"].values
 
@@ -117,61 +120,3 @@ def gearboxa():
 # run the app
 if __name__ == '__main__':
     app.run()
-
-# Take user input for brand
-print("Available brands: ", unique_brands)
-brand = input("Enter brand: ")
-
-# Extract the rows where the value in the "Brand" column is equal to the desired brand
-brand_df = df.loc[df["Brand"] == brand]
-
-# Get unique models for the chosen brand
-unique_models = np.unique(brand_df['Model'])
-
-# Take user input for model
-print("Available models for brand ", brand, ": ", unique_models)
-model = input("Enter model: ")
-
-# Extract the rows where the value in the "Model" column is equal to the desired model
-model_df = brand_df.loc[brand_df["Model"] == model]
-
-# Take user input for price range
-price_min = float(input("Enter minimum price: "))
-price_max = float(input("Enter maximum price: "))
-
-# Extract the rows where the value in the "Price" column is between the price range
-price_range_df = model_df.loc[(model_df["Price"] >= price_min) & (model_df["Price"] <= price_max)]
-
-# Display the search result
-print(price_range_df)
-
-
-columns = df.columns
-
-print("The number of samples in dataset: {}".format(df.shape[0]))
-for column in columns:
-    print(column, " unique size: {}".format(df[column].unique().size))
-    
-    if df[column].unique().size == df.shape[0]:
-        print(column, ": Unnecessary Column. Delete from the dataset.")
-        df = df.drop(labels=column, axis=1)
-
-# using matplotlib
-
-# Get the top 20 companies
-company_top20 = (df.groupby('Brand').size().reset_index(name='counts').sort_values(by='counts', ascending=False).head(20))
-#
-plt.figure(figsize=(18, 5))
-sns.barplot(x=company_top20.Brand, y=company_top20.counts) #, palette='Set2')
-plt.xlabel('Company Name')
-plt.ylabel('Number of cars')
-plt.xticks(rotation=45)
-plt.show()
-
-# write html to let the user select the brand
-html = df.to_html('converted_file.html')   
-brands = df['Brand'].unique()
-brands = pd.DataFrame(brands)
-brands.to_html('brands.html')
-
-(df.loc[df['Brands'] == 'input'])
